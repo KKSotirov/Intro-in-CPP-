f754ff3611bb867d098e81062c76c4a214d4d5e9
KKSotirov <sotirov.k73@gmail.com>
2026-08-09 23:36:44 +0300

    Added more dynamic matrix excercises

---
+++ Dynamic matrixes/dyn_matrixes_3.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+
+int **transposeMatrix(const int *const *matrix, unsigned rows, unsigned cols)
+{
+    int **transposedMatrix = new int *[cols];
+    for (size_t i = 0; i < cols; i++)
+    {
+        transposedMatrix[i] = new int[rows];
+        for (size_t j = 0; j < rows; j++)
+        {
+            transposedMatrix[i][j] = matrix[j][i];
+        }
+    }
+
+    return transposedMatrix;
+}
+
+void freeMatrix(int **matrix, unsigned rows)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        delete[] matrix[i];
+    }
+    delete[] matrix;
+}
+
+void printMatrix(const int *const *matrix, unsigned rows, unsigned cols)
+{
+    for (size_t i = 0; i < rows; i++)
+    {
+        for (size_t j = 0; j < cols; j++)
+        {
+            std::cout << matrix[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main()
+{
+    unsigned rows, cols;
+    std::cout << "Please enter number of rows: " << std::endl;
+    std::cin >> rows;
+    std::cout << "Please enter number of cols: " << std::endl;
+    std::cin >> cols;
+
+    // 1. Заделяме памет и четем оригиналната матрица от конзолата
+    int **matrix = new int *[rows];
+    std::cout << "Enter matrix elements:" << std::endl;
+    for (size_t i = 0; i < rows; i++)
+    {
+        matrix[i] = new int[cols];
+        for (size_t j = 0; j < cols; j++)
+        {
+            std::cin >> matrix[i][j];
+        }
+    }
+
+    // 2. Транспонираме, като подаваме оригиналната матрица
+    int **transposedMatrix = transposeMatrix(matrix, rows, cols);
+
+    std::cout << "Transposed Matrix:" << std::endl;
+    printMatrix(transposedMatrix, cols, rows);
+
+    // 3. Почистваме паметта и на двете матрици
+    freeMatrix(matrix, rows);
+    freeMatrix(transposedMatrix, cols);
+
+    return 0;
+}
